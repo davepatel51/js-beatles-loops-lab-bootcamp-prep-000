419f8b64d0c1df4eec570d0579847ea2b162381e
Dave Patel <ddp2134@gmail.com>
2020-02-17 06:36:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ function iLoveTheBeatles(num) {
     } while (num < 15);
     return love;
 }
+
+
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+    return allPlayers;
+}
