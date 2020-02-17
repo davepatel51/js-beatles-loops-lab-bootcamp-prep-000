e435f9317a4cf14b5a6942a0f3cd1746a84dddf5
Dave Patel <ddp2134@gmail.com>
2020-02-17 06:35:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
+}
